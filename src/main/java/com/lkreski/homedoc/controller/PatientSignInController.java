e59ca40da6d634255365ad5650b596a534216196
lukkre002 <lukaszcezarykreski@gmail.com>
2019-01-19 14:44:58 +0100

    [ADD] Dodano zależność do kodowania sha-256, zahardowano płatność dotpay (będzie trzeba poprawić z hardkoda)

--- src/main/java/com/lkreski/homedoc/controller/PatientSignInController.java
+++ src/main/java/com/lkreski/homedoc/controller/PatientSignInController.java
@@ -1,5 +1,6 @@
 package com.lkreski.homedoc.controller;
 
+import com.google.common.hash.Hashing;
 import com.lkreski.homedoc.model.HomeVisit;
 import com.lkreski.homedoc.service.HomeVisitService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 public class PatientSignInController {
@@ -22,8 +27,14 @@ public class PatientSignInController {
 
 
     @RequestMapping("/patientSignIn")
-    public String signIn(Model model){
+    public String signIn(Model model) throws NoSuchAlgorithmException {
         model.addAttribute("homeVisit", new HomeVisit());
+        String originalString = "PfyPuBxoowYLeuCGqQwIj59JUTzEqa3w"+"dev"+"723751"+"322.00"+"PLN"+"aaac";
+
+        String sha256hex = Hashing.sha256()
+                .hashString(originalString, StandardCharsets.UTF_8)
+                .toString();
+        String result = "https://ssl.dotpay.pl/test_payment/?api_version=dev&id=723751&kwota=322.00&waluta=PLN&opis=aaac&chk="+sha256hex;
         return "patientsignin";
     }
 
@@ -31,7 +42,9 @@ public class PatientSignInController {
     public String addNewVisit(@Valid HomeVisit homeVisit, BindingResult result, Model model){
         System.out.println(homeVisit);
         homeVisitService.addVisit(homeVisit);
-        return "patientsignin";
+        return "patientsigninStep2";
     }
+
+
 
 }

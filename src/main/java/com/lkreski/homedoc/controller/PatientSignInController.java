fe9e461c343526dbfb05af7306cb96a027963805
lukkre002 <lukaszcezarykreski@gmail.com>
2019-01-19 18:06:12 +0100

    [ADD] Dodany service obsługi dotpay + kolejne popraki dotpay

--- src/main/java/com/lkreski/homedoc/controller/PatientSignInController.java
+++ src/main/java/com/lkreski/homedoc/controller/PatientSignInController.java
@@ -1,7 +1,9 @@
 package com.lkreski.homedoc.controller;
 
+import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
 import com.lkreski.homedoc.model.HomeVisit;
+import com.lkreski.homedoc.service.DotpayService;
 import com.lkreski.homedoc.service.HomeVisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,24 +19,43 @@ import javax.validation.Valid;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 @Controller
 public class PatientSignInController {
 
     @Autowired
      HomeVisitService homeVisitService;
+    @Autowired
+    DotpayService dotpayService;
 
 
 
     @RequestMapping("/patientSignIn")
     public String signIn(Model model) throws NoSuchAlgorithmException {
         model.addAttribute("homeVisit", new HomeVisit());
-        String originalString = "PfyPuBxoowYLeuCGqQwIj59JUTzEqa3w"+"dev"+"723751"+"322.00"+"PLN"+"aaac";
 
-        String sha256hex = Hashing.sha256()
-                .hashString(originalString, StandardCharsets.UTF_8)
-                .toString();
-        String result = "https://ssl.dotpay.pl/test_payment/?api_version=dev&id=723751&kwota=322.00&waluta=PLN&opis=aaac&chk="+sha256hex;
+        HashMap<String,String> checkerValues = new HashMap<>();
+        checkerValues.put("PIN","PfyPuBxoowYLeuCGqQwIj59JUTzEqa3w");
+        checkerValues.put("api_version","dev");
+        checkerValues.put("id","723751");
+        checkerValues.put("amount","322.00");
+        checkerValues.put("currency","PLN");
+        checkerValues.put("description","aaac");
+        checkerValues.put("ignore_last_payment_channel","1");
+        checkerValues.put("ignore_last_payment_channel","1");
+        checkerValues.put("channel_groups","K,T,P,M");
+        checkerValues.put("url","http://localhost:8080/patientSignIn/step2");
+        checkerValues.put("type","0");
+        checkerValues.put("buttontext","Wróc do HomeDoc");
+        String s = dotpayService.generateChk(checkerValues);
+        String hashCode = Hashing.sha256().hashString(s, StandardCharsets.UTF_8).toString();
+
+        String result2 = "https://ssl.dotpay.pl/test_payment/?api_version=dev&id=723751&kwota=322.00&waluta=PLN&opis=aaac" +
+                "&ignore_last_payment_channel=1&grupykanalow=K,T,P,M&URL=http://localhost:8080/patientSignIn/step2&typ=0&txtguzik=Wróc do HomeDoc&chk="+hashCode;
+
+
+
         return "patientsignin";
     }
 
@@ -42,6 +63,30 @@ public class PatientSignInController {
     public String addNewVisit(@Valid HomeVisit homeVisit, BindingResult result, Model model){
         System.out.println(homeVisit);
         homeVisitService.addVisit(homeVisit);
+        HashMap<String,String> checkerValues = new HashMap<>();
+        checkerValues.put("PIN","PfyPuBxoowYLeuCGqQwIj59JUTzEqa3w");
+        checkerValues.put("api_version","dev");
+        checkerValues.put("id","723751");
+        checkerValues.put("amount","322.00");
+        checkerValues.put("currency","PLN");
+        checkerValues.put("description","aaac");
+        checkerValues.put("ignore_last_payment_channel","1");
+        checkerValues.put("ignore_last_payment_channel","1");
+        checkerValues.put("channel_groups","K,T,P,M");
+        checkerValues.put("url","http://localhost:8080/patientSignIn/step2");
+        checkerValues.put("type","0");
+        checkerValues.put("buttontext","Wróc do HomeDoc");
+        String s = dotpayService.generateChk(checkerValues);
+        String hashCode = Hashing.sha256().hashString(s, StandardCharsets.UTF_8).toString();
+
+        String result2 = "https://ssl.dotpay.pl/test_payment/?api_version=dev&id=723751&kwota=322.00&waluta=PLN&opis=aaac" +
+                "&ignore_last_payment_channel=1&grupykanalow=K,T,P,M&URL=http://localhost:8080/patientSignIn/step2&typ=0&txtguzik=Wróc do HomeDoc&chk="+hashCode;
+        return "redirect:"+result2;
+    }
+
+    @RequestMapping(value = "/patientSignIn/step2", method = RequestMethod.GET)
+    public String patientSigninStep2(){
+
         return "patientsigninStep2";
     }
 
